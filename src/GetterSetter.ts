f9e483e87e0e9392072bcfebeefe24333e14f410
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-20 14:42:42 +0530

    getter and setters created

--- src/GetterSetter.ts
+++ src/GetterSetter.ts
@@ -9,6 +9,30 @@ class Customer {
         this.cAddress = cAddress;
     }
 
+    setCId(cId : number) {
+        this.cId = cId;
+    }
+
+    getCId():number {
+        return this.cId;
+    }
+
+    setCName(cName: string) {
+        this.cName = cName;
+    }
+
+    getCName(): string {
+        return this.cName;
+    }
+
+    setCAddress(cAddress: string) {
+        this.cAddress = cAddress;
+    }
+
+    getCAddress(): string {
+        return this.cAddress
+    }
+
     print(): void {
         console.log(this.cName, ":", this.cAddress);
     }
@@ -17,3 +41,17 @@ class Customer {
 
 const customer1 = new Customer(1,"Shenan","Panadura");
 customer1.print();
+
+console.log(customer1.getCId());
+console.log(customer1.getCName());
+console.log(customer1.getCAddress());
+
+customer1.setCId(1);
+customer1.setCName("Shenan Vindinu");
+customer1.setCAddress("USA");
+
+console.log("\n");
+
+console.log(customer1.getCId());
+console.log(customer1.getCName());
+console.log(customer1.getCAddress());
